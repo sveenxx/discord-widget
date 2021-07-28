b5f5dc0970522cb5d268b6b56f69f6dc13a96c57
sveenxx <sveenv2@gmail.com>
2021-07-28 20:27:11 +0200

    Remove unnecessary code

--- dist/browser/index.js
+++ dist/browser/index.js
@@ -1,2 +1,2 @@
-
+var t,i,e,s;t=module.exports,i="Widget",e=()=>n,Object.defineProperty(t,i,{get:e,set:s,enumerable:!0,configurable:!0});class n{constructor(t,i){this.element=t,this.guild_id=i,this.init()}async init(){const t=`https://discord.com/api/guilds/${this.guild_id}/widget.json`,i=await async function(t){try{return await(await fetch(t)).json()}catch(t){throw new Error(t)}}(t);console.log(i)}}
 //# sourceMappingURL=index.js.map
