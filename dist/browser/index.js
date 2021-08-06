e12708d8ccd54a0021e902522dfa3514d4f91d0b
sveenxx <sveenv2@gmail.com>
2021-08-06 16:54:37 +0200

    [build 1.1.2]

--- dist/browser/index.js
+++ dist/browser/index.js
@@ -1,2 +1,71 @@
-var e,t,o,r,n;e=module.exports,Object.defineProperty(e,"__esModule",{value:!0,configurable:!0}),t=module.exports,o="default",r=()=>s,Object.defineProperty(t,o,{get:r,set:n,enumerable:!0,configurable:!0});class i{constructor(e,t,o){this.element=e,this.options=o,this.guild=t;const r=document.createElement("div");var n,i;r.setAttribute("data-discord-widget","true"),n=r,i={color:"white",backgroundColor:"gray",width:"400px",height:"500px",borderRadius:"15px"},Object.keys(i).forEach((e=>{n.style[e.split(/(?=[A-Z])/).join("-").toLowerCase()]=i[e]})),this.element.appendChild(r),console.log("123")}}class s extends i{static async init(e,t,o){const r=await async function(e){try{const t=`https://discord.com/api/guilds/${e}/widget.json`;return await(await fetch(t)).json()}catch(e){throw new Error(e)}}(t);if(!e||1!==e.nodeType)throw new TypeError(`exptect element to be DOM Element, but got ${e}`);if(!r)throw new TypeError(`exptect element to be Object, but got ${r}`);return new i(e,r,o)}}s.version="1.0.0";
+async function $944e66fb72f7e12f$export$336ad58b686ca37(id) {
+    try {
+        const URL1 = `https://discord.com/api/guilds/${id}/widget.json`;
+        const data = await (await fetch(URL1)).json();
+        return data;
+    } catch (err) {
+        throw new Error(err);
+    }
+}
+
+
+function $cd453b592329dacd$export$9d050de78026ecc9(el, styles) {
+    Object.keys(styles).forEach((prop)=>{
+        el.style[prop.split(/(?=[A-Z])/).join("-").toLowerCase()] = styles[prop];
+    });
+}
+
+
+
+
+
+class $0f2e64b2dafab7ac$export$6da0c54d420de4fd {
+    constructor(element, guild, options){
+        this.element = element;
+        this.options = options;
+        this.guild = guild;
+        /**
+         * Create html element for this widget
+         */ const contentElement = document.createElement("div");
+        /**
+         * Mark as discord widget
+         */ contentElement.setAttribute("data-discord-widget", "true");
+        $cd453b592329dacd$export$9d050de78026ecc9(contentElement, {
+            color: "white",
+            backgroundColor: "gray",
+            width: "400px",
+            height: "500px",
+            borderRadius: "15px"
+        });
+        this.element.appendChild(contentElement);
+        console.log("123");
+    }
+}
+
+
+const $dba23c4f427410e2$var$WIDGET_VERSION = "1.0.0";
+class $dba23c4f427410e2$export$9099ad97b570f7c extends $0f2e64b2dafab7ac$export$6da0c54d420de4fd {
+    /**
+     * Initializes the widget on the specified element
+     *
+     * @param element The DOM element to initialize
+     * @param guildId Guild identifier
+     * @param options [optional] Options for the widget
+     * @returns
+     */ static async init(element, guildId, options) {
+        const guild = await $944e66fb72f7e12f$export$336ad58b686ca37(guildId);
+        if (!element || element.nodeType !== 1) throw new TypeError(`exptect element to be DOM Element, but got ${element}`);
+        if (!guild) throw new TypeError(`exptect element to be Object, but got ${guild}`);
+        return new $0f2e64b2dafab7ac$export$6da0c54d420de4fd(element, guild, options);
+    }
+}
+$dba23c4f427410e2$export$9099ad97b570f7c.version = $dba23c4f427410e2$var$WIDGET_VERSION;
+(async ()=>{
+    const test = document.getElementById("discord");
+    const widget = await $dba23c4f427410e2$export$9099ad97b570f7c.init(test, "600381707073486871");
+    console.log(widget);
+})();
+
+
+export {$dba23c4f427410e2$export$9099ad97b570f7c as default};
 //# sourceMappingURL=index.js.map
--- dist/index.js
+++ dist/index.js
@@ -1,2 +1,71 @@
-class t{constructor(t,e,o){this.element=t,this.options=o,this.guild=e;const r=document.createElement("div");var n,s;r.setAttribute("data-discord-widget","true"),n=r,s={color:"white",backgroundColor:"gray",width:"400px",height:"500px",borderRadius:"15px"},Object.keys(s).forEach((t=>{n.style[t.split(/(?=[A-Z])/).join("-").toLowerCase()]=s[t]})),this.element.appendChild(r),console.log("123")}}class e extends t{static async init(e,o,r){const n=await async function(t){try{const e=`https://discord.com/api/guilds/${t}/widget.json`;return await(await fetch(e)).json()}catch(t){throw new Error(t)}}(o);if(!e||1!==e.nodeType)throw new TypeError(`exptect element to be DOM Element, but got ${e}`);if(!n)throw new TypeError(`exptect element to be Object, but got ${n}`);return new t(e,n,r)}}e.version="1.0.0";export{e as default};
+async function $4d64ccfd0b96f38d$export$336ad58b686ca37(id) {
+    try {
+        const URL1 = `https://discord.com/api/guilds/${id}/widget.json`;
+        const data = await (await fetch(URL1)).json();
+        return data;
+    } catch (err) {
+        throw new Error(err);
+    }
+}
+
+
+function $ad78cece2d354e89$export$9d050de78026ecc9(el, styles) {
+    Object.keys(styles).forEach((prop)=>{
+        el.style[prop.split(/(?=[A-Z])/).join("-").toLowerCase()] = styles[prop];
+    });
+}
+
+
+
+
+
+class $4b03852290fffcba$export$6da0c54d420de4fd {
+    constructor(element, guild, options){
+        this.element = element;
+        this.options = options;
+        this.guild = guild;
+        /**
+         * Create html element for this widget
+         */ const contentElement = document.createElement("div");
+        /**
+         * Mark as discord widget
+         */ contentElement.setAttribute("data-discord-widget", "true");
+        $ad78cece2d354e89$export$9d050de78026ecc9(contentElement, {
+            color: "white",
+            backgroundColor: "gray",
+            width: "400px",
+            height: "500px",
+            borderRadius: "15px"
+        });
+        this.element.appendChild(contentElement);
+        console.log("123");
+    }
+}
+
+
+const $29132bf5159c333f$var$WIDGET_VERSION = "1.0.0";
+class $29132bf5159c333f$export$9099ad97b570f7c extends $4b03852290fffcba$export$6da0c54d420de4fd {
+    /**
+     * Initializes the widget on the specified element
+     *
+     * @param element The DOM element to initialize
+     * @param guildId Guild identifier
+     * @param options [optional] Options for the widget
+     * @returns
+     */ static async init(element, guildId, options) {
+        const guild = await $4d64ccfd0b96f38d$export$336ad58b686ca37(guildId);
+        if (!element || element.nodeType !== 1) throw new TypeError(`exptect element to be DOM Element, but got ${element}`);
+        if (!guild) throw new TypeError(`exptect element to be Object, but got ${guild}`);
+        return new $4b03852290fffcba$export$6da0c54d420de4fd(element, guild, options);
+    }
+}
+$29132bf5159c333f$export$9099ad97b570f7c.version = $29132bf5159c333f$var$WIDGET_VERSION;
+(async ()=>{
+    const test = document.getElementById("discord");
+    const widget = await $29132bf5159c333f$export$9099ad97b570f7c.init(test, "600381707073486871");
+    console.log(widget);
+})();
+
+
+export {$29132bf5159c333f$export$9099ad97b570f7c as default};
 //# sourceMappingURL=index.js.map
