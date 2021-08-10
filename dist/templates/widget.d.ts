253f4c00a5ad5a1d5a1209fc1ed6d7bbe5d8e1a4
sveenxx <sveenv2@gmail.com>
2021-08-10 23:05:22 +0200

    Improve file structure

---
+++ dist/templates/widget.d.ts
@@ -0,0 +1,1 @@
+export declare const getWidget: (guild: any) => any;
---
+++ src/templates/widgetMemberList.ts
@@ -0,0 +1,27 @@
+import { template } from "../template";
+
+export const getMemeberList = (guild) =>
+    template(guild, (data) =>
+        data.members.map(
+            (member) =>
+                `
+    <div class="widget-member">                         
+        <div>
+            <div class="widget-member-avatar">
+                <img src="${member.avatar_url}"
+                    alt="">
+                <span class="widget-member-status" data-status="${
+                    member.status
+                }"></span>
+            </div>
+            <span class="widget-member-name">${member.username}</span>
+        </div>
+        ${
+            member.game === undefined
+                ? ``
+                : `<span class="widget-member-desc">${member.game.name}</span>`
+        }
+    </div>
+`
+        )
+    );
